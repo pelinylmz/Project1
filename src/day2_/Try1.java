6ecca17a9fe5cee560fa8d61ea7ae131e9ea42b5
pelinylmz <62036371+pelinylmz@users.noreply.github.com>
2020-05-12 23:57:01 +0200

    Update Try1.java

--- src/day2_/Try1.java
+++ src/day2_/Try1.java
@@ -2,6 +2,7 @@ package day2_;
 
 public class Try1 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello,hello");
+        
     }
 }
